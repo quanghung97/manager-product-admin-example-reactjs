890349d81b2987143624b1ba5d2b2f975c826831
quanghung97 <hungcan1997@gmail.com>
2019-05-11 15:06:56 +0700

    apply React-Router P1

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import React, {Component} from 'react'
 import './App.css'
 import Menu from './components/Menu/Menu'
 import ProductList from './components/ProductList/ProductList'
+import routes from './routes'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
 class App extends Component {
+
+  showContentMenus = (routes) => {
+    let result = null
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return <Route key={index} path={route.path} exact={route.exact} component={route.main} />
+      })
+    }
+    return <Switch>{ result }</Switch>
+  }
+
   render() {
     return (
-      <div>
-        <Menu />
-        <div className="container">
-          <button className="btn btn-info mb-2">
-            Thêm sản phẩm
-          </button>
-          <ProductList />
+      <Router>
+        <div>
+          <Menu />
+          <div className="container">
+            {/* <button className="btn btn-info mb-2">
+              Thêm sản phẩm
+            </button>
+            <ProductList /> */}
+
+            { this.showContentMenus(routes) }
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
